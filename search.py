dd0bf89863fdf8bc7fd66a69223cb5c5d6aaae5e
ameniawy <ameniawy@bitbucket.com>
2017-02-02 13:18:35 +0200

    search.py not complete

---
+++ search.py
@@ -0,0 +1,17 @@
+# Abdelrahman M.
+
+import searchable_index
+
+
+LOAD = 0
+
+def __init__():
+	
+
+
+def _start_index():
+	"""
+	 Calls the function tokenize from searchable_index to get an inverse dict of the data
+	"""
+
+	index_dict = searchable_index.tokenize()
