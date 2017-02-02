989d2367f9a562ad09fd3b019c514d4bb677768e
ameniawy <ameniawy@bitbucket.com>
2017-02-02 16:48:03 +0200

    some additions to searchable_index.py

--- searchable_index.py
+++ searchable_index.py
@@ -7,10 +7,10 @@ from collections import defaultdict
 def _add_to_inverse_index(index_dict, tokens):
 	"""
 	Adds new tokens to inverse index.
-	Params:
+	Args:
 		index_dict: dict{word:list(ids)}
 		tokens: tuple(id, list(words))
-	returns:
+	Returns:
 		index_dict
 	"""
 	article_id = tokens[0]
@@ -25,33 +25,58 @@ def _add_to_inverse_index(index_dict, tokens):
 	return index_dict
 
 
-def _load_tsv(path):
-	"""
-	Loads tsv file.
-	Params: path(str): path to file
+def _load_tsv(file_path):
+    """
+	Read tsv files.
+	Args:
+		file_path (str): path to the file to be read
 	Returns:
+		list[list(str)]: content of the tsv file
 	"""
+	with open(file_path) as f:
+		reader = csv.reader(f, delimiter='\t', quoting=csv.QUOTE_NONE)
+		content = [row for row in reader]
+
+	return content
+
+
+def _load_content(file_path):
+	"""
+	Calls _load_tsv and organizes the data in a dict format
+	Args:
+		file_path (str): path to the file to be read
+	Returns:
+		articles_dict (dict{id: text}) : contains id: text of each row in the corpus
+
+	"""
+	two_dim = _load_tsv(file_path)
+	articles_dict = {}
+
+	for row in two_dim:
+		# row[0] = id, row[1] = title, row[2] = text
+		text_id = row[0]
+
+		# append the title and the text together
+		text = row[1] + ' ' + row[2]
+		articles_dict[text_id] = text
+
+	return articles_dict
 
 
 def tokenize():
 	"""
 	Load and tokenize the tsv file
+	Returns:
+		index_dict (dict{word:list(ids)}) : contains the inverse indices
 	"""
 
-	articles = load_tsv("simplewiki.tsv") # dict{id: text}
+	articles = _load_content("simplewiki.tsv") # dict{id: text}
 	index_dict = defaultdict(list)
 
 	for article in articles:
 		text = articles[article]
 		words = extract(text, "en")
-		index_dict = add_to_inverse_index(index_dict, words)
+		index_dict = _add_to_inverse_index(index_dict, (article, words))
 
 	return index_dict
-
-
-
-
-
-
-
 
